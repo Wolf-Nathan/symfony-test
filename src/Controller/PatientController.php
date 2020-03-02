91f93bc0cbb379da197591ad14728c6dad87a933
Wolf Nathan <nathan-w@hotmail.fr>
2020-03-02 11:04:45 +0100

    Make Patient CRUD

--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -2,59 +2,93 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Patient;
-use App\Entity\Consultation;
+use App\Form\PatientType;
+use App\Repository\PatientRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/patient")
+ */
 class PatientController extends AbstractController
 {
     /**
-     * @Route("/patient", name="patient")
+     * @Route("/", name="patient_index", methods={"GET"})
      */
-    public function index()
+    public function index(PatientRepository $patientRepository): Response
     {
-        $patient = new Patient;
-        //Hydratation de l'instance de l'entité Patient
-        $patient->setNumSS('168077511115369');
-        $patient->setNom('Dupond');
-        $patient->setPrenom('Jacques');
-        $maDate = new \DateTime('1978-07-18');
-        $patient->setDateNaissance($maDate);
-        $patient->setSexe('M');
-
-        // Créations de consultations
-        $consultation = new Consultation;
-        $heureConsult = new \DateTime('now');
-        $consultation->setDateHeure($heureConsult);
-        $consultation->setPatient($patient);
-
-        $patient->addConsultation($consultation);
-
-        // Récupération du service Doctrine.
-        $doctrine = $this->getDoctrine();
-        // Récupération du service gestionnaire d'entités
-        $entityManager = $doctrine->getManager();
-        $entityManager->persist($consultation);
-        $entityManager->persist($patient);
-        $entityManager->flush();
-
         return $this->render('patient/index.html.twig', [
-            'controller_name' => 'Nouveau patient créé',
+            'patients' => $patientRepository->findAll(),
         ]);
     }
 
     /**
-     * @Route("/patient/list", name="patient_list")
+     * @Route("/new", name="patient_new", methods={"GET","POST"})
      */
-    public function list(){
-        $patientRepository = $this->getDoctrine()->getManager()->getRepository(Patient::class);
-        $patients = $patientRepository->findAll();
+    public function new(Request $request): Response
+    {
+        $patient = new Patient();
+        $form = $this->createForm(PatientType::class, $patient);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($patient);
+            $entityManager->flush();
 
-        return $this->render('patient/list.html.twig', [
-            'controller_name' => 'Liste des patients',
-            'patients' => $patients
+            return $this->redirectToRoute('patient_index');
+        }
+
+        return $this->render('patient/new.html.twig', [
+            'patient' => $patient,
+            'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/{id}", name="patient_show", methods={"GET"})
+     */
+    public function show(Patient $patient): Response
+    {
+        return $this->render('patient/show.html.twig', [
+            'patient' => $patient,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="patient_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Patient $patient): Response
+    {
+        $form = $this->createForm(PatientType::class, $patient);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('patient_index');
+        }
+
+        return $this->render('patient/edit.html.twig', [
+            'patient' => $patient,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="patient_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Patient $patient): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$patient->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($patient);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('patient_index');
     }
 }
