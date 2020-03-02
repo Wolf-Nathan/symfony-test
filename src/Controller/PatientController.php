66ea84b6f193b1134eb7fd75f1c0aff4b25a2d5c
Wolf Nathan <nathan-w@hotmail.fr>
2020-03-02 09:32:00 +0100

    Modif patientController to add patient and his consultation

--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Patient;
+use App\Entity\Consultation;
 
 class PatientController extends AbstractController
 {
@@ -12,8 +14,33 @@ class PatientController extends AbstractController
      */
     public function index()
     {
+        $patient = new Patient;
+        //Hydratation de l'instance de l'entité Patient
+        $patient->setNumSS('168077511115369');
+        $patient->setNom('Dupond');
+        $patient->setPrenom('Jacques');
+        $maDate = new \DateTime('1978-07-18');
+        $patient->setDateNaissance($maDate);
+        $patient->setSexe('M');
+
+        // Créations de consultations
+        $consultation = new Consultation;
+        $heureConsult = new \DateTime('now');
+        $consultation->setDateHeure($heureConsult);
+        $consultation->setPatient($patient);
+
+        $patient->addConsultation($consultation);
+
+        // Récupération du service Doctrine.
+        $doctrine = $this->getDoctrine();
+        // Récupération du service gestionnaire d'entités
+        $entityManager = $doctrine->getManager();
+        $entityManager->persist($consultation);
+        $entityManager->persist($patient);
+        $entityManager->flush();
+
         return $this->render('patient/index.html.twig', [
-            'controller_name' => 'PatientController',
+            'controller_name' => 'Nouveau patient créé',
         ]);
     }
 }
