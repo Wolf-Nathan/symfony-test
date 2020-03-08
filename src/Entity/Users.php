c772cd2e52bafc36ece3eff8255d53186891a995
Wolf Nathan <nathan-w@hotmail.fr>
2020-03-08 14:42:20 +0100

    Add user's property name

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -35,6 +35,11 @@ class Users implements UserInterface
      */
     private $password;
 
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $name;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -111,5 +116,17 @@ class Users implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
     }
 }
